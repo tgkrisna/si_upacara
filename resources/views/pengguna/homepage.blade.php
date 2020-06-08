be1c8048a4428c0d32bb33d7a81d55859df0af8e
tgkrisna <tgkrisna@gmail.com>
2020-06-08 23:33:24 +0800

    Swiper didnt want automatic

--- resources/views/pengguna/homepage.blade.php
+++ resources/views/pengguna/homepage.blade.php
@@ -4,7 +4,7 @@
 <div class="container py-5">
     <h1 class="text-center mb-5 text-white">E-Upacara</h1>
     <form action="#" class="form mb-5">
-        <div class="input-grup">
+        <div class="input-group">
             <input class="form-control" placeholder="Cari..." name="cari">
             <span class="input-group-append">
                 <button type="submit" class="btn btn-success">
@@ -13,14 +13,33 @@
             </span>
         </div>
     </form>
-    <div class="swiper-container">
-        @foreach($data['kategori'] as $kat)
+    <div class="swiper-container" id="swiper-menu">
         <div class="swiper-wrapper">
+            @foreach($data['kategori'] as $kat)
             <div class="swiper-slide">
-                {{$kat->nama_kategori}}
+                <div class="card card-upacara gradient-1 text-center">
+                    <div class="card-body">
+                        <h1 class="card-title my-5">
+                            <a href="#" class="stretched-link">
+                                {{$kat->nama_kategori}}
+                            </a>
+                        </h1>
+                    </div>
+                </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
 </div>
+<script src="{{asset('/assets/swiper/dist/js/swiper.js')}}">
+var mySwiper = new Swiper('#swiper-menu', {
+    speed: 400,
+    spaceBetween: 100,
+    effect: 'slide',
+    slidesPerView: 'auto'
+});
+    var mySwiper = document.querySelector('#swiper-menu').swiper
+
+    mySwiper.slideNext();
+</script>
 @endsection
