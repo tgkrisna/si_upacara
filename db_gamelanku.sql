fb123135b8c295dff4323c94a75d207915026067
tgkrisna <tgkrisna@gmail.com>
2020-06-13 01:37:51 +0800

    New Database

--- db_gamelanku.sql
+++ db_gamelanku.sql
@@ -42,6 +42,7 @@ CREATE TABLE `tb_detil_post` (
   `id_tag` int(11) DEFAULT NULL,
   `id_post` int(11) DEFAULT NULL,
   `id_parent_post` int(11) DEFAULT NULL,
+  `id_tingkatan` int(11) DEFAULT NULL,
   `id_status` int(11) DEFAULT NULL,
   `spesial` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_det_post`),
@@ -49,19 +50,23 @@ CREATE TABLE `tb_detil_post` (
   KEY `id_tag` (`id_tag`),
   KEY `id_status` (`id_status`),
   KEY `id_parent_post` (`id_parent_post`),
+  KEY `id_tingkatan` (`id_tingkatan`),
   CONSTRAINT `tb_detil_post_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `tb_post` (`id_post`),
   CONSTRAINT `tb_detil_post_ibfk_2` FOREIGN KEY (`id_tag`) REFERENCES `tb_tag` (`id_tag`),
   CONSTRAINT `tb_detil_post_ibfk_3` FOREIGN KEY (`id_status`) REFERENCES `tb_status` (`id_status`),
-  CONSTRAINT `tb_detil_post_ibfk_4` FOREIGN KEY (`id_parent_post`) REFERENCES `tb_post` (`id_post`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  CONSTRAINT `tb_detil_post_ibfk_4` FOREIGN KEY (`id_parent_post`) REFERENCES `tb_post` (`id_post`),
+  CONSTRAINT `tb_detil_post_ibfk_5` FOREIGN KEY (`id_tingkatan`) REFERENCES `tb_tingkatan` (`id_tingkatan`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tb_detil_post` */
 
-insert  into `tb_detil_post`(`id_det_post`,`id_tag`,`id_post`,`id_parent_post`,`id_status`,`spesial`) values 
-(1,3,4,5,1,4),
-(2,3,5,6,NULL,4),
-(3,1,5,1,NULL,4),
-(4,3,5,7,NULL,4);
+insert  into `tb_detil_post`(`id_det_post`,`id_tag`,`id_post`,`id_parent_post`,`id_tingkatan`,`id_status`,`spesial`) values 
+(1,3,4,5,NULL,1,4),
+(2,3,5,6,NULL,NULL,4),
+(3,1,5,1,NULL,NULL,4),
+(4,3,5,7,1,NULL,4),
+(5,2,1,2,NULL,NULL,NULL),
+(6,1,4,1,NULL,NULL,4);
 
 /*Table structure for table `tb_kategori` */
 
@@ -99,20 +104,19 @@ CREATE TABLE `tb_post` (
   KEY `tb_post_ibfk_2` (`id_kategori`),
   CONSTRAINT `tb_post_ibfk_1` FOREIGN KEY (`id_tag`) REFERENCES `tb_tag` (`id_tag`),
   CONSTRAINT `tb_post_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `tb_kategori` (`id_kategori`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tb_post` */
 
 insert  into `tb_post`(`id_post`,`id_kategori`,`id_tag`,`nama_post`,`deskripsi`,`video`,`gambar`) values 
-(1,NULL,1,'Gamelan A','Gamelan A',NULL,NULL),
-(2,NULL,2,'Tari B','Tari B',NULL,NULL),
+(1,2,1,'Gamelan A','<p>Gamelan A adalah bla bla the fox shit on the ggoru. gg wep aads sdmas sdasd</p>','9lVPAWLWtWc','1589440847_download.jfif'),
+(2,2,2,'Tari B','<p>Tari Bali bla bla&nbsp;</p>','tlILlcCE8Sc','1590672284_ao.jpg'),
 (3,NULL,1,'Gamelan C','Gamelan C',NULL,NULL),
-(4,3,NULL,'Upacara A','Upacara A',NULL,NULL),
-(5,NULL,3,'Prosesi A','Prosesi A',NULL,NULL),
+(4,3,NULL,'Upacara A','<p>Upacara A</p>','https://www.youtube.com/watch?v=9lVPAWLWtWc','1588595450_ciri - ciri ayam bangkok juara-11.jpg'),
+(5,2,3,'Prosesi A','<p>Prosesi A&nbsp;<span style=\"background-color: #ffffff; color: #212529; font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, \'Helvetica Neue\', Arial, \'Noto Sans\', sans-serif, \'Apple Color Emoji\', \'Segoe UI Emoji\', \'Segoe UI Symbol\', \'Noto Color Emoji\'; font-size: 16px;\">Piodalan yang utamanya sebagai kelompok upacara Dewa Yadnya ini merupakan upacara yang ditujukan kehadapan Ida Sang Hyang Widhi Wa&ccedil;a dengan segala manifestasinya yang pujawalinya dipimpin oleh seorang pemangku di tempat suci masing-masing.</span></p>','9lVPAWLWtWc','1591239710_anti misugi.PNG'),
 (6,NULL,3,'Prosesi B','Prosesi B',NULL,NULL),
 (7,NULL,3,'Prosesi C','Prosesi C',NULL,NULL),
-(8,2,NULL,'Tes',NULL,'https://www.youtube.com/watch?v=9lVPAWLWtWc',NULL),
-(9,2,NULL,'Tes 2',NULL,'https://www.youtube.com/watch?v=9lVPAWLWtWc',NULL),
+(8,2,NULL,'Tes Yubi','<p>Yosh</p>','9lVPAWLWtWc','1588595472_ahhsh.PNG'),
 (10,2,NULL,'Tes 3',NULL,'https://www.youtube.com/watch?v=9lVPAWLWtWc',NULL),
 (11,3,NULL,'Tes 123',NULL,'https://www.youtube.com/watch?v=9lVPAWLWtWc',NULL),
 (12,2,NULL,'Tes 2222','<p>tes222222</p>','https://www.youtube.com/watch?v=9lVPAWLWtWc',NULL),
@@ -123,7 +127,9 @@ insert  into `tb_post`(`id_post`,`id_kategori`,`id_tag`,`nama_post`,`deskripsi`,
 (17,2,NULL,'yorushika','<p>yorushika</p>','9lVPAWLWtWc','1588422018_download.jfif'),
 (18,2,NULL,'lol','<p>lol</p>','9lVPAWLWtWc','1588504793_Yorushika_Logo.jpg'),
 (19,2,NULL,'achillea','<p>achillea</p>','9lVPAWLWtWc','1588593596_artworks-000409460535-61sens-t500x500.jpg'),
-(20,2,NULL,'Dummy1','<p>Dummy1</p>','9lVPAWLWtWc','Kategori_2');
+(20,2,NULL,'Dummy1','<p>Dummy1</p>','9lVPAWLWtWc','1588594979_ere.PNG'),
+(21,NULL,1,'Skirller','<p>Skriller</p>','9lVPAWLWtWc','1588677608_heh.PNG'),
+(22,3,1,'Uwu','<p>Uueue</p>','9lVPAWLWtWc','1588678078_ere.PNG');
 
 /*Table structure for table `tb_status` */
 
@@ -152,15 +158,37 @@ CREATE TABLE `tb_tag` (
   `deskripsi` text,
   `gambar` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_tag`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tb_tag` */
 
 insert  into `tb_tag`(`id_tag`,`nama_tag`,`deskripsi`,`gambar`) values 
-(1,'Gamelan','Gamelan',NULL),
+(1,'Gamelan Bali','Gamelan Bali',NULL),
 (2,'Tari Bali','Tari Bali',NULL),
 (3,'Prosesi Upacara','Prosesi Upacara',NULL),
-(4,'Tabuh','Tabuh',NULL);
+(4,'Kidung','Kidung',NULL),
+(5,'Tingkatan Upacara','Tingkatan Upacara',NULL);
+
+/*Table structure for table `tb_tingkatan` */
+
+DROP TABLE IF EXISTS `tb_tingkatan`;
+
+CREATE TABLE `tb_tingkatan` (
+  `id_tingkatan` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tag` int(11) DEFAULT NULL,
+  `nama_tingkatan` varchar(50) DEFAULT NULL,
+  `deskripsi` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_tingkatan`),
+  KEY `id_tag` (`id_tag`),
+  CONSTRAINT `tb_tingkatan_ibfk_1` FOREIGN KEY (`id_tag`) REFERENCES `tb_tag` (`id_tag`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_tingkatan` */
+
+insert  into `tb_tingkatan`(`id_tingkatan`,`id_tag`,`nama_tingkatan`,`deskripsi`) values 
+(1,5,'Uttama','Uttama'),
+(2,5,'Madya','Madya'),
+(3,5,'Kanista','Kanista');
 
 /*Table structure for table `users` */
 
